fa0f7943782ce5b32f82de112e1cb5afa86c0c20
Piyush Agrawal <poush12@gmail.com>
2018-02-18 19:13:11 +0530

    Form added

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-    <login></login>
+    <!-- <login></login> -->
+    <el-row>
+      <el-col :span="24">
+      <men></men>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top:50px">
+    <el-col :span="12" :offset="6">
+        <formy></formy>
+    </el-col>
+    </el-row>
   </div>
 </template>
 <script>
-  import Login from './components/Login.vue';
+
+  import Formy from './components/Form.vue';
+  import Men from './components/Menu.vue';
 
   export default {
     name: 'app',
     components: {
-      Login
+      Formy, Men
     }
   };
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Example</title>
+  <title>ICMR</title>
 </head>
 <body>
   <div id="app"></div>
